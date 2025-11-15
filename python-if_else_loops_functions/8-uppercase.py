6e2c583e6deeafcaf9bcfbc25dfba76937ff1833
root <root@kali>
2025-11-15 22:54:25 +0400

    1

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for i in range(len(str)):
+        if ord(str[i]) >= 97:
+            result += str[i].swapcase()
+        else:
+            result += str[i]
+    print("{}".format(result))
